5c7d83591de655cdf6a3f7ba5e10873646fd1d82
DuncanRL <59705125+DuncanRuns@users.noreply.github.com>
2021-08-07 20:01:51 -0300

    1.14.4 ResidentSleeper

--- src/main/java/me/duncanruns/autoreset/AutoReset.java
+++ src/main/java/me/duncanruns/autoreset/AutoReset.java
@@ -9,6 +9,7 @@ public class AutoReset implements ModInitializer {
     public static final String MOD_ID = "autoreset";
     public static final String MOD_NAME = "Auto Reset Mod";
     public static boolean isPlaying = false;
+    public static boolean loopPrevent = false;
     public static Logger LOGGER = LogManager.getLogger();
 
     public static void log(Level level, String message) {
